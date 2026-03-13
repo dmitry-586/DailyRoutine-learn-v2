1561df61e0d4d4badd1a98314b5dec16e506080d
dmitry kutsevalov <kutdmitry@gmail.com>
2026-03-13 13:10:40 +0300

    фикс стилей в оглавлении теории

--- src/app/providers/NavTheory.tsx
+++ src/app/providers/NavTheory.tsx
@@ -60,7 +60,7 @@ export function NavTheory() {
           </div>
         </section>
       )}
-      <div className='sticky bottom-5 left-0 z-20 flex gap-3 sm:gap-5'>
+      <div className='sticky bottom-5 left-0 z-20 flex w-fit gap-3 sm:gap-5'>
         <HomeButton className='size-11 p-2.5' />
         <BurgerButton
           className='size-11 p-2.5'
