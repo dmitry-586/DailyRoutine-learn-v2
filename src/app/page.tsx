c52ac0c306b94c69aa3ba342ad974a0418593d15
dmitry kutsevalov <kutdmitry@gmail.com>
2026-03-17 10:56:09 +0300

    фикс favicon.ico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import { AllCards } from '@/features'
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center flex-1'>
+    <div className='flex flex-1 flex-col justify-center'>
       <section className='mt-5 flex flex-col'>
         <div className='text-center'>
           <h1 id='main-heading' className='text-xl font-bold sm:text-2xl'>
             Daily Routine <span className='text-primary'>Learn</span>
           </h1>
-          <p className='mt-2 text-sm text-white/70 sm:text-base'>Выберите режим обучения</p>
+          <p className='mt-2 text-sm text-white/70 sm:text-base'>
+            Выберите режим обучения
+          </p>
         </div>
       </section>
       <AllCards />
